4d1b8196e43750589c947dca212ac4c1d082a9ac
chief_andrew <chief.andrew5891@gmail.com>
2026-09-15 12:08:45 +0200

    last update 15/09/2026 12:08

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         applicationId = "com.andrey.beautyplanner"
         minSdk = 24
         targetSdk = 36
-        versionCode = 11
-        versionName = "1.3.6"
+        versionCode = 12
+        versionName = "1.4.0"
     }
 
     compileOptions {
